210531469bec7eae183109f324a6835186065579
Worood Abdou <w@pravica.io>
2025-05-20 20:20:03 +0300

    Chatt and Main app Modifications

--- backend/db.php
+++ backend/db.php
@@ -1,1 +1,16 @@
+<?php
+$host = 'localhost';
+$dbname = 'isychia';  // Replace with your DB name
+$username = 'root';
+$password = ''; // Default password is empty in XAMPP
 
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("❌ Connection failed: " . $conn->connect_error);
+}
+
+//echo "✅ Connected successfully to MySQL!";
+?>
